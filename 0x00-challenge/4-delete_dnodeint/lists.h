6107fc22a5ea8d26f6a15fc8c8dbd40e5aa7c370
1r2o3b4e5l <robelamsalu44@gmail.com>
2024-06-02 03:02:37 -0700

    delete_dnodeint_at_index.c

--- 0x00-challenge/4-delete_dnodeint/lists.h
+++ 0x00-challenge/4-delete_dnodeint/lists.h
@@ -1,12 +1,10 @@
-#ifndef _LISTS_H_
-#define _LISTS_H_
-
-#include <stddef.h>
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
- * @prev: points to the prev node
+ * @prev: points to the previous node
  * @next: points to the next node
  *
  * Description: doubly linked list node structure
@@ -14,14 +12,15 @@
  */
 typedef struct dlistint_s
 {
-	int n;
-	struct dlistint_s *prev;
-	struct dlistint_s *next;
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
 } dlistint_t;
 
-size_t print_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
+size_t print_dlistint(const dlistint_t *h);
 void free_dlistint(dlistint_t *head);
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif
+#endif /* LISTS_H */
+
